0522e159c2620be7d7445dd42baef9b46ab233e6
AlgorithmSolver <qu9qu7@gmail.com>
2022-06-15 11:13:33 +0900

    -

--- Language/Javascript/ClassStudy/intro.js
+++ Language/Javascript/ClassStudy/intro.js
@@ -117,5 +117,94 @@
 }
 
 {
-  //constructor
+  // constructor
+  class Point {
+    constructor(point) {
+      this.point = point;
+    }
+  }
+  const obj = new Point();
+  obj.setPoint(100);
+}
+
+{
+  // Number 반환
+  class Point {
+    constructor(point) {
+      this.point = point;
+      return point;
+    }
+  }
+  const obj = new point(100);
+  console.log(obj.point);
+  console.log(obj instanceof Point); // true
+}
+
+{
+  // Object 반환
+  class Point {
+    constructor(point) {
+      return { point: point }; // 오브젝트 표현식 반환
+    }
+  }
+  const obj = new point(100);
+  console.log(obj);
+  console.log(obj instanceof Point);
+  // Point로 만든 인스턴스가 아니기 때문에 false
+}
+
+// getter, setter, static메소드
+{
+  //getter, setter
+  class Point {
+    constructor(point) {
+      this.point = point;
+    }
+    get getPoint() {
+      return this.point;
+    }
+    set setPoint(point) {
+      this.point = point;
+    }
+  }
+  const obj = new Point(0);
+  obj.setPoint(120);
+}
+
+{
+  //static 메소드 - 프로토타입이 아닌 클래스에 바로 연결
+  class Point {
+    constructor(point) {
+      this.point = point;
+    }
+    static getPoint() {
+      return 100;
+    }
+  }
+  const obj = new Point();
+  console.log(obj.getPoint);
+}
+
+{
+  // 클래스는 호이스팅이 되지 않음
+}
+
+// new.target
+// - 함수 또는 생성자가 new 연산자로 호출된 여부 반환
+{
+  // constructor를 호출
+  class Point {
+    constructor() {
+      console.log(new.target.name);
+    }
+  }
+  new Point();
+}
+
+{
+  // 함수는 undefined 반환
+  function book() {
+    console.log(new.target);
+  }
+  book();
 }
